21319df07d19ec8588ae3696e76a41cf2836699e
cgmolive <cassiusgremaschi@hotmail.com>
2019-07-03 22:11:12 -0300

    Adicionados primeiros arquivos para persistencia

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@ namespace SistemaDeEstacionamentos
     {
         static void Main(string[] args)
         {
+
+
+            
+        }
+
+        private static void gravarUsandoEntity()
+        {
+            Usuarios u = new Usuarios("Cassius", "Rua fictícia","Jardim do sei lá das quantas","Complemento", 309, "123455678", "usuario", "senha"; 
+            using (var usuarios = new UsuariosContext())
+            {
+                usuarios.Usuarios.Add(u);
+                usuarios.SaveChanges();
+            }
         }
     }
 }
