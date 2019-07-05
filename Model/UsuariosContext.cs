c030f1af57007efdc9c92edf58d4671399ff29ee
Cassius Gremaschi Marcelino de Oliveira <cassius.oliveira@benner.com.br>
2019-07-05 16:23:35 -0300

    Adicionado folders para modelo MVC

---
+++ Model/UsuariosContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace SistemaDeEstacionamentos
+{
+    internal class UsuariosContext : DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}
---
+++ Model/Usuários.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeEstacionamentos
+{
+    public class Usuarios
+    {
+        static int sequencial = 1;
+        private CPF cpf { get; private set; }
+        private Nome nome { get; private set; }
+        private Endereco endereco { get; private set; }
+        private CredenciaisDeAcesso credenciaisDeAcesso;
+        private long handler;
+
+        public Usuarios (string nome, string Logradouro, string Bairro, string Complemento, int numero, string Valor, string usuario, string senha)
+        {
+            this.handler += sequencial;
+            this.cpf = new CPF(Valor);
+            this.nome = new Nome(nome);
+            this.endereco = new Endereco(Logradouro, Bairro, Complemento, numero);
+            this.credenciaisDeAcesso = new CredenciaisDeAcesso(usuario, senha);
+
+        }
+
+        
+
+        
+
+    }
+}
