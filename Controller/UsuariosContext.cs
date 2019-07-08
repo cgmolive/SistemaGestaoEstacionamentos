b625d11434d48709cd4bcb6f878f8da2d9b65f41
cgmolive <cassiusgremaschi@hotmail.com>
2019-07-08 19:41:44 -0300

    Inserida validação para CPF

---
+++ Controller/UsuariosContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace SistemaDeEstacionamentos
+{
+    internal class UsuariosContext : DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(postgreSQL)\\mssqllocaldb;Database=SistemaGestaoEstacionamentos;Trusted_Connection=true;");
+        }
+    }
+}
