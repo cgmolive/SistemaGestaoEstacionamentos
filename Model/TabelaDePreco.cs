16ebc297fd8e71f5db067fd87d47e663e649ad71
cgmolive <cassiusgremaschi@hotmail.com>
2019-07-24 11:13:58 -0300

    Adicionada tabela de preços

---
+++ Model/TabelaDePreco.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaGestaoEstacionamentos.Model
+{
+    public class TabelaDePreco
+    {
+        public float AteQuatroHoras { get; set; }
+        public float AdicionalPorHora { get; set; }
+        public float ValorDiaria { get; set; }
+
+        public TabelaDePreco(float AteQuatroHoras, float AdicionalPorHora, float ValorDiaria)
+        {
+            this.AteQuatroHoras = AteQuatroHoras;
+            this.AdicionalPorHora = AdicionalPorHora;
+            this.ValorDiaria = ValorDiaria
+        }
+
+        public float calculaPrecoTicket(DateTime HorarioDeEntrada, DateTime HorarioDeSaida)
+        {
+            float valorFinal;
+            TimeSpan TempoDecorrido = HorarioDeEntrada.Subtract(HorarioDeSaida);
+
+            //Decisão do valor pago
+            //Por padrão, vou assumir que a diária custa mais que 8 horas, e definir como limite.
+            if (TempoDecorrido.Hours <= 4)
+            {
+                valorFinal = AteQuatroHoras;
+            }
+            else if (TempoDecorrido.Hours >= 8)
+            {
+                valorFinal = ValorDiaria;
+            }
+            else
+            {
+                valorFinal = AteQuatroHoras + ((TempoDecorrido.Hours - 4) * AdicionalPorHora);
+            }
+            return valorFinal;
+        }
+    }
+}
