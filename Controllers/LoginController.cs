f03460e2498ac7a2811abc22dabf720cd668bcca
cgmolive <cassiusgremaschi@hotmail.com>
2019-08-30 09:49:37 -0300

    Ajustes de estilizacao

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -17,6 +17,22 @@ namespace SistemaGestaoEstacionamentos.Controllers
             return View();
         }
 
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            Exception exception = filterContext.Exception;
+            //Logging the Exception
+            filterContext.ExceptionHandled = true;
+
+
+            var Result = this.View("Error", new HandleErrorInfo(exception,
+                filterContext.RouteData.Values["controller"].ToString(),
+                filterContext.RouteData.Values["action"].ToString()));
+
+            filterContext.Result = Result;
+
+        }
+
+
         public ActionResult AutenticaLogin(string login, string senha)
         {
             UsuariosDAO dao = new UsuariosDAO();
--- Controllers/VagasController.cs
+++ Controllers/VagasController.cs
@@ -20,6 +20,23 @@ namespace SistemaGestaoEstacionamentos.Controllers
             ViewBag.EstacionamentoId = Handle;
             return View();
         }
+
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            Exception exception = filterContext.Exception;
+            //Logging the Exception
+            filterContext.ExceptionHandled = true;
+
+
+            var Result = this.View("Error", new HandleErrorInfo(exception,
+                filterContext.RouteData.Values["controller"].ToString(),
+                filterContext.RouteData.Values["action"].ToString()));
+
+            filterContext.Result = Result;
+
+        }
+
+
         [HttpGet]
         public ActionResult CadastrarVaga(int Handle)
         {
